7db80a20dabd2947a17241ea2c6ebf2cca04a609
Michael Lynch <git@mtlynch.io>
2025-11-16 11:39:30 -0500

    Inline values.js

--- creator/values.js
+++
@@ -1,9 +0,0 @@
-const secretType = "'{.SECRET_TYPE}'"
-const secretExt  = "'{.SECRET_EXTENSION}'"
-const saltSize   = '{.SALT_SIZE}'  // bytes
-const blockSize  = '{.BLOCK_SIZE}' // bytes
-const keySize    = '{.KEY_SIZE}'   // bytes
-const iterations = '{.ITERATIONS}'
-const saltHex    = "'{.SALT_HEX}'"
-const ivHex      = "'{.IV_HEX}'"
-const cipherHex  = "'{.CIPHER_HEX}'"
